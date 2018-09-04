61bcc9c8374a9bf6e3327624382954caee387ad3
Adam Jacobus <adam.jacobus@uqconnect.edu.au>
2018-09-04 14:27:47 +1000

    Add slug functionality to API

--- backend/follyengine/folly_api/views.py
+++ backend/follyengine/folly_api/views.py
@@ -20,12 +20,30 @@ class GroupViewSet(viewsets.ModelViewSet):
     """
     queryset = Group.objects.all()
     serializer_class = serializers.GroupSerializer
-    permission_classes = (permissions.IsAuthenticated,)
+    permission_classes = (permissions.IsAdminUser,)
 
 
 class ProjectViewSet(viewsets.ModelViewSet):
+    """
+    retrieve:
+    Get more details about a specific project. This includes entities,
+    components, flows, and assets.
+
+    list:
+    Gets all projects owned by the authenticated user, ordered by modification
+    date.
+
+    create:
+    Creates a new project, with the authenticated user as the owner.
+    """
     serializer_class = serializers.ProjectSerializer
     permission_classes = (permissions.IsAuthenticated,)
+
+    def get_serializer_class(self):
+        serializer = self.serializer_class
+        if self.request.method == 'POST':
+            serializer = serializers.ProjectCreateSerializer
+        return serializer
 
     def get_queryset(self):
         return self.request.user.projects.all().order_by('-modified')
