96bec115397ffcdb7755ff088a16ec0b1bec6cea
Trent <trentonsmyth@hotmail.com>
2018-09-10 23:27:02 +1000

    Created timestamp and author tests for django project model

--- backend/follyengine/folly_api/tests.py
+++ backend/follyengine/folly_api/tests.py
@@ -1,3 +1,28 @@
-from django.test import TestCase
+from rest_framwork import TestCaseAPI
+from follyengine.folly_api.models import Project
+from django.contrib.auth.models import User
+from datetime import datetime
 
-# Create your tests here.
+class ProjectModelTest(TestCaseAPI):
+
+    @classmethod
+    def setupTestData(cls):
+        cls.user = User.objects.create(username="projectOwner")
+        cls.project = Project.objects.create(title="testProject", description="testFieldDescription", owner=cls.user)
+
+        @TestCaseAPI
+        def test_project_timestamps(self):
+            time = datetime.now()
+            project = Project.objects.get(title="testProject")
+            project_created_time = project.created.datetime()
+            project_modified_time = project.modified.datetime()
+            delta = datetime.timedelta(seconds=2)
+            TestCaseAPI.assertTrue(time - project_created_time < delta)
+            TestCaseAPI.asserTrue(time - project_modified_time < delta)
+
+
+        @TestCaseAPI
+        def test_project_author(self):
+            project = Project.objects.get(title="testProject")
+            user = User.objects.get(username="projectOwner")
+            TestCaseAPI.asserEquals(project.owner , user)
