d9eca5f8f2aa743b522d736005d8253084628529
Trenton Smyth <trentonsmyth@hotmail.com>
2018-09-11 13:16:34 +1000

    Refactored testing and moved to correct path

---
+++ backend/follyengine/folly_api/tests/test_project_model.py
@@ -0,0 +1,28 @@
+from rest_framework.test import TestCaseAPI
+from follyengine.folly_api.models import Project
+from django.contrib.auth.models import User
+from datetime import datetime
+
+class ProjectModelTest(TestCaseAPI):
+
+    @classmethod
+    def setupTestData(cls):
+        cls.user = User.objects.create(username="projectOwner")
+        cls.project = Project.objects.create(title="testProject",
+                                             description="testFieldDescription",
+                                             owner=cls.user)
+
+        def test_project_timestamps(self):
+            time = datetime.now()
+            project = self.project
+            project_created_time = project.created.datetime()
+            project_modified_time = project.modified.datetime()
+            delta = datetime.timedelta(seconds=2)
+            self.assertTrue(time - project_created_time < delta)
+            self.assertTrue(time - project_modified_time < delta)
+
+
+        def test_project_author(self):
+            project = self.project
+            user = self.user
+            self.assertEquals(project.owner , user)
