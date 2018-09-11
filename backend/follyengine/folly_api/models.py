e5867cb8ab55b563e2f1192c3ea24ff0f2e4a848
Trenton Smyth <trentonsmyth@hotmail.com>
2018-09-11 13:22:23 +1000

    Fix merge conflicts

--- backend/follyengine/folly_api/models.py
+++ backend/follyengine/folly_api/models.py
@@ -5,6 +5,7 @@ from django.dispatch import receiver
 from rest_framework.authtoken.models import Token
 
 
+
 # Create your models here.
 class Project(models.Model):
     title = models.CharField(max_length=64)
@@ -17,7 +18,6 @@ class Project(models.Model):
 
     def __str__(self):
         return self.title
-
 
 @receiver(post_save, sender=settings.AUTH_USER_MODEL)
 def create_auth_token(sender, instance=None, created=False, **kwargs):
