6871bdb7f7ad9b301333a1f8c977b2c8f31c9200
Adam Jacobus <adam.jacobus@uqconnect.edu.au>
2018-09-11 11:33:00 +1000

    Add tests for token authentication endpoint.

--- backend/follyengine/folly_api/tests/test_auth_token.py
+++ backend/follyengine/folly_api/tests/test_auth_token.py
@@ -5,26 +5,49 @@ from rest_framework.test import APITestCase
 
 
 class AuthTokenTest(APITestCase):
+    url = '/api/auth/token'
+
     @classmethod
     def setUpTestData(cls):
-        cls.user = User.objects.create(username='testuser',
-                                       email='test@test.com',
-                                       password='thisisatestpassword')
+        cls.user = User.objects.create_user('testuser',
+                                            password='thisisatestpassword')
 
     def test_successful_auth(self):
-        url = '/api/auth/token'
         data = {'username': 'testuser', 'password': 'thisisatestpassword'}
-        response = self.client.post(url, data, format='json')
+        response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.accepted_media_type, 'application/json')
-        self.assertIn(response.content, 'token')
-        self.assertEqual(response.content['token'], self.user.token)
+        self.assertEqual(response.data['token'], self.user.auth_token.key)
 
     def test_empty_request_fails(self):
-        pass
+        response = self.client.post(self.url, data=None, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {
+            'username': ['This field is required.'],
+            'password': ['This field is required.']
+        })
 
     def test_blank_fields_fails(self):
-        pass
+        data = {'username': '', 'password': ''}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {
+            'username': ['This field may not be blank.'],
+            'password': ['This field may not be blank.']
+        })
 
     def test_invalid_user_fails(self):
-        pass
+        data = {'username': 'notauser', 'password': 'password'}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {
+            'non_field_errors': ['Unable to log in with provided credentials.']
+        })
+
+    def test_incorrect_password_fails(self):
+        data = {'username': 'testuser', 'password': 'password'}
+        response = self.client.post(self.url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {
+            'non_field_errors': ['Unable to log in with provided credentials.']
+        })
