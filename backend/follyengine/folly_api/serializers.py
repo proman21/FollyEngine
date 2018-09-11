bca387f5e439095816e3339163896b81639a53a7
Adam Jacobus <adam.jacobus@uqconnect.edu.au>
2018-09-11 14:21:39 +1000

    Rebase from master.

--- backend/follyengine/folly_api/serializers.py
+++ backend/follyengine/folly_api/serializers.py
@@ -19,4 +19,26 @@ class GroupSerializer(serializers.ModelSerializer):
 class ProjectSerializer(serializers.ModelSerializer):
     class Meta:
         model = models.Project
-        fields = ('title', 'description', 'slug', 'created', 'modified')
+        fields = ('url', 'title', 'description', 'slug', 'created', 'modified',
+                  'owner')
+
+    slug = serializers.SlugField(read_only=True)
+    owner = serializers.PrimaryKeyRelatedField(
+        read_only=True,
+        default=serializers.CurrentUserDefault())
+
+
+class ProjectCreateSerializer(ProjectSerializer):
+    slug = serializers.SlugField(required=False)
+
+
+class EntitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Entity
+        fields = ('name', 'description', 'components')
+
+
+class ComponentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Component
+        fields = ('name', 'description')
