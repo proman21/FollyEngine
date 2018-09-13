dc5344997cfe8a1b7f418ee2d2b98f175b7bd780
Adam Jacobus <adam.jacobus@uqconnect.edu.au>
2018-09-13 11:38:32 +1000

    Format code to Python standard

--- backend/follyengine/folly_api/tests/test_project_model.py
+++ backend/follyengine/folly_api/tests/test_project_model.py
@@ -3,13 +3,14 @@ from follyengine.folly_api.models import Project
 from django.contrib.auth.models import User
 from datetime import datetime
 
+
 class ProjectModelTest(TestCaseAPI):
 
     @classmethod
     def setupTestData(cls):
         cls.user = User.objects.create(username="projectOwner")
         cls.project = Project.objects.create(title="testProject",
-                                             description="testFieldDescription",
+                                             description="testDescription",
                                              owner=cls.user)
 
         def test_project_timestamps(self):
@@ -21,8 +22,7 @@ class ProjectModelTest(TestCaseAPI):
             self.assertTrue(time - project_created_time < delta)
             self.assertTrue(time - project_modified_time < delta)
 
-
         def test_project_author(self):
             project = self.project
             user = self.user
-            self.assertEquals(project.owner , user)
+            self.assertEquals(project.owner, user)
