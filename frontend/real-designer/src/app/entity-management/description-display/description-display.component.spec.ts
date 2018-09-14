c2d77eabe7071a4bbcc3c5166029cb1df81cd80e
Max Crofts <m.crofts@uqconnect.edu.au>
2018-09-14 15:33:10 +1000

    Restore description display

---
+++ frontend/real-designer/src/app/entity-management/description-display/description-display.component.spec.ts
@@ -0,0 +1,7 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DescriptionDisplayComponent } from './description-display.component';
+
+describe('DescriptionDisplayComponent', () => {
+
+});
