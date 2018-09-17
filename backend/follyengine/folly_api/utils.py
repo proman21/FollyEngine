976f77e46c91c9a30218b5dab2f210920daa4a63
Adam Jacobus <adam.jacobus@uqconnect.edu.au>
2018-09-17 22:15:36 +1000

    Remove broken JSONSchemaField

--- backend/follyengine/folly_api/utils.py
+++ backend/follyengine/folly_api/utils.py
@@ -1,6 +1,4 @@
 from django.utils.text import slugify
-from voluptuous import Schema, MultipleInvalid
-from rest_framework.serializers import ValidationError, JSONField
 
 
 class SlugDefault(object):
@@ -13,16 +11,3 @@ class SlugDefault(object):
     def set_context(self, field):
         self._field = field.parent.initial_data[self.source]
         print(self._field)
-
-
-class JSONSchemaField(JSONField):
-    def __init__(self, schema, **kwargs):
-        self.schema = Schema(schema)
-        super().__init__(self, **kwargs)
-
-    def to_internal_value(self, data):
-        parsed = super().to_internal_value(self, data)
-        try:
-            return self.schema(parsed)
-        except MultipleInvalid as e:
-            raise ValidationError(str(e))
