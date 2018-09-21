ba2281b5832f9fc53bd198d049ffba30acfd2a40
Max Crofts <m.crofts@uqconnect.edu.au>
2018-09-21 16:07:33 +1000

    Remove DescriptionDisplayComponent

--- frontend/real-designer/src/app/entity-management/entity-management.module.ts
+++ frontend/real-designer/src/app/entity-management/entity-management.module.ts
@@ -11,7 +11,6 @@ import { MaterialModule } from './../material.module';
 import { SharedUiModule } from '../shared-ui/shared-ui.module';
 import { EntityEditorComponent } from './entity-editor/entity-editor.component';
 import { ComponentDisplayComponent } from './component-display/component-display.component';
-import { DescriptionDisplayComponent } from './description-display/description-display.component';
 import { ProceduresDisplayComponent } from './procedures-display/procedures-display.component';
 
 @NgModule({
@@ -22,7 +21,6 @@ import { ProceduresDisplayComponent } from './procedures-display/procedures-disp
     EntityManagementComponent,
     EntityEditorComponent,
     ComponentDisplayComponent,
-    DescriptionDisplayComponent,
     ProceduresDisplayComponent
   ]
 })
