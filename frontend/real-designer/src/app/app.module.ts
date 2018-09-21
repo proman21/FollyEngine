255279747ddc501423e49bd36baa56cb187650d2
Max Crofts <m.crofts@uqconnect.edu.au>
2018-09-21 13:31:22 +1000

    Add missing comma

--- frontend/real-designer/src/app/app.module.ts
+++ frontend/real-designer/src/app/app.module.ts
@@ -42,7 +42,7 @@ import { AuthInterceptor } from './auth/auth.interceptor';
     HttpClientModule
   ],
   providers: [
-    DesignerService
+    DesignerService,
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
